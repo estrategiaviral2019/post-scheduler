324419f4f0d3775d68e44cbeb77fb170190ca54a
estrategiaviral2019 <estrategiaviral2019@gmail.com>
2026-04-08 19:26:29 -0500

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import TwitterProvider from "next-auth/providers/twitter";
-import RedditProvider from "next-auth/providers/reddit";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prisma";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID as string,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-      version: "2.0",
-    }),
-    RedditProvider({
-      clientId: process.env.REDDIT_CLIENT_ID as string,
-      clientSecret: process.env.REDDIT_CLIENT_SECRET as string,
-      authorization: { params: { duration: "permanent" } },
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      if (session?.user) {
-        // @ts-ignore
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
